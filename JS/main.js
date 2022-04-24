0c25309adc8100666d7f7b4a5eb3bb7f0c3f8f93
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2022-04-24 19:14:44 -0400

    Missed a spot

--- JS/main.js
+++ JS/main.js
@@ -2,7 +2,5 @@ window.addEventListener('load', setFooterDate, false);
 
 function setFooterDate() {
     let footerDate = document.getElementById("footerDate");
-    let date = new Date().getFullYear();
-
-    footerDate.textContent = date.toString();
+    footerDate.textContent = new Date().getFullYear().toString();
 }
