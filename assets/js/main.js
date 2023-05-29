69a448fb639a7791e4f12fb5ece726611bcd4ccc
NinjaCheetah <58050615+NinjaCheetah@users.noreply.github.com>
2023-05-28 23:29:26 -0400

    Updated to Bootstrap v5.3 Alpha and added a dark theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,19 @@
 window.addEventListener('load', setFooterDate, false);
+window.addEventListener('load', loadTheme, false);
 
 function setFooterDate() {
     let footerDate = document.getElementById("footerDate");
     footerDate.textContent = new Date().getFullYear().toString();
+}
+
+// Load saved theme automatically on load
+function loadTheme() {
+    var theme = localStorage.getItem('theme');
+    document.documentElement.setAttribute('data-bs-theme', theme)
+}
+
+// Set the theme using the navbar item
+function chooseTheme(theme){
+    document.documentElement.setAttribute('data-bs-theme', theme)
+    localStorage.setItem('theme', theme);
 }
